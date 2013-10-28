5fa853582719292ccc52291a4a48dc90244b814b
plnegre <plnegre@binibook.com>
2013-10-28 13:05:38 +0100

    Changed the solvePnP direction. Now 3d points are computed from the template image

--- include/template_pose_base.h
+++ include/template_pose_base.h
@@ -8,7 +8,6 @@
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <image_transport/subscriber_filter.h>
-#include <image_geometry/stereo_camera_model.h>
 #include <sensor_msgs/image_encodings.h>
 #include <tf/transform_broadcaster.h>
 #include "opencv2/core/core.hpp"
@@ -41,14 +40,25 @@ public:
     string template_image_name;       //!> Name of the template image located at the node path into '/etc' directory (example: template.png)
     int min_matches;                  //!> Minimum number of matches to estimate the transform between template and stereo frame.
     int min_inliers;                  //!> Minimum number of inliers to considerate the transform valid.
-    string stereo_frame_id;           //!> Frame id for the stereo camera.
+    string frame_id;                  //!> Frame id for the stereo camera.
     string template_frame_id;         //!> Frame id for the template image.
+    string desc_type;                 //!> Descriptor type can be: SIFT or SURF.
+    int bucket_width;                 //!> Bucket width.
+    int bucket_height;                //!> Bucket height.
+    int max_bucket_features;          //!> Maximum number the features per bucket.
+    double template_width;            //!> The template image width in meters.
+    double template_height;           //!> The template image height in meters.
 
     // Default values
-    static const int          DEFAULT_QUEUE_SIZE = 1;
-    static const double       DEFAULT_DESC_THRESHOLD = 0.8;
-    static const int          DEFAULT_MIN_MATCHES = 70;
-    static const int          DEFAULT_MIN_INLIERS = 25;
+    static const int            DEFAULT_QUEUE_SIZE = 1;
+    static const double         DEFAULT_DESC_THRESHOLD = 0.8;
+    static const int            DEFAULT_MIN_MATCHES = 70;
+    static const int            DEFAULT_MIN_INLIERS = 25;
+    static const int            DEFAULT_BUCKET_WIDTH = 30;
+    static const int            DEFAULT_BUCKET_HEIGHT = 30;
+    static const int            DEFAULT_MAX_BUCKET_FEATURES = 10;
+    static const double         DEFAULT_TEMPLATE_WIDTH = 0.5;
+    static const double         DEFAULT_TEMPLATE_HEIGHT = 0.16;
   };
 
   // Set the parameter struct
@@ -61,14 +71,12 @@ protected:
 
 	// Node handlers
 	ros::NodeHandle nh_;
-  ros::NodeHandle nh_private_;
+  ros::NodeHandle nhp_;
 
   bool initialize();
   void readParameters();
-  void msgsCallback(const sensor_msgs::ImageConstPtr& l_img,
-                    const sensor_msgs::ImageConstPtr& r_img,
-                    const sensor_msgs::CameraInfoConstPtr& l_info,
-                    const sensor_msgs::CameraInfoConstPtr& r_info);
+  void msgsCallback(const sensor_msgs::ImageConstPtr& img,
+                    const sensor_msgs::CameraInfoConstPtr& img_info);
 
 private:
 
@@ -76,17 +84,13 @@ private:
   bool estimateTransform(tf::Transform& output);
 
   // Topic properties
-  image_transport::SubscriberFilter left_sub_, right_sub_;
-  message_filters::Subscriber<sensor_msgs::CameraInfo> left_info_sub_, right_info_sub_;
+  image_transport::SubscriberFilter image_sub_;
+  message_filters::Subscriber<sensor_msgs::CameraInfo> info_sub_;
 
   // Topic sync properties
   typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, 
-                                                    sensor_msgs::Image, 
-                                                    sensor_msgs::CameraInfo, 
                                                     sensor_msgs::CameraInfo> ExactPolicy;
   typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, 
-                                                    sensor_msgs::Image, 
-                                                    sensor_msgs::CameraInfo, 
                                                     sensor_msgs::CameraInfo> ApproximatePolicy;
   typedef message_filters::Synchronizer<ExactPolicy> ExactSync;
   typedef message_filters::Synchronizer<ApproximatePolicy> ApproximateSync;
@@ -100,7 +104,7 @@ private:
   tf::Transform camera_to_template_;          //!> Transform from camera to template
   tf::TransformBroadcaster tf_broadcaster_;   //!> Transform publisher
   ImageProperties template_prop_;             //!> Image properties for the template image
-  ImageProperties stereo_prop_;               //!> Image properties for the stereo frame
+  ImageProperties img_prop_;               //!> Image properties for the stereo frame
   Mat camera_matrix_;                         //!> Camera matrix
   bool first_iter_;                           //!> True in the first iteration
 };
--- src/template_pose_base.cpp
+++ src/template_pose_base.cpp
@@ -12,7 +12,13 @@ template_pose::TemplatePoseBase::Params::Params() :
   queue_size(DEFAULT_QUEUE_SIZE),
   desc_threshold(DEFAULT_DESC_THRESHOLD),
   min_matches(DEFAULT_MIN_MATCHES),
-  min_inliers(DEFAULT_MIN_INLIERS)
+  min_inliers(DEFAULT_MIN_INLIERS),
+  desc_type("SIFT"),
+  bucket_width(DEFAULT_BUCKET_WIDTH),
+  bucket_height(DEFAULT_BUCKET_HEIGHT),
+  max_bucket_features(DEFAULT_MAX_BUCKET_FEATURES),
+  template_width(DEFAULT_TEMPLATE_WIDTH),
+  template_height(DEFAULT_TEMPLATE_HEIGHT)
 {}
 
 /** \brief Class constructor. Reads node parameters and initialize some properties.
@@ -20,7 +26,7 @@ template_pose::TemplatePoseBase::Params::Params() :
   * \param nhp private node handler
   */
 template_pose::TemplatePoseBase::TemplatePoseBase(
-  ros::NodeHandle nh, ros::NodeHandle nhp) : nh_(nh), nh_private_(nhp)
+  ros::NodeHandle nh, ros::NodeHandle nhp) : nh_(nh), nhp_(nhp)
 {
   // Read the node parameters
   readParameters();
@@ -33,22 +39,17 @@ template_pose::TemplatePoseBase::TemplatePoseBase(
   * messages are received.
   * @return 
   * \param l_img ros image message of type sensor_msgs::Image
-  * \param r_img ros image message of type sensor_msgs::Image
   * \param l_info ros info image message of type sensor_msgs::CameraInfo
-  * \param r_info ros info image message of type sensor_msgs::CameraInfo
   */
 void template_pose::TemplatePoseBase::msgsCallback(
-                                  const sensor_msgs::ImageConstPtr& l_img,
-                                  const sensor_msgs::ImageConstPtr& r_img,
-                                  const sensor_msgs::CameraInfoConstPtr& l_info,
-                                  const sensor_msgs::CameraInfoConstPtr& r_info)
+                                  const sensor_msgs::ImageConstPtr& img,
+                                  const sensor_msgs::CameraInfoConstPtr& img_info)
 {
   // Convert image message to Mat
-  cv_bridge::CvImagePtr l_ptr, r_ptr;
+  cv_bridge::CvImagePtr img_ptr;
   try
   {
-    l_ptr = cv_bridge::toCvCopy(l_img, enc::BGR8);
-    r_ptr = cv_bridge::toCvCopy(r_img, enc::BGR8);
+    img_ptr = cv_bridge::toCvCopy(img, enc::BGR8);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -59,21 +60,15 @@ void template_pose::TemplatePoseBase::msgsCallback(
   if(first_iter_)
   {
     // Initialize camera matrix    
-    image_geometry::StereoCameraModel stereo_camera_model;
-    stereo_camera_model.fromCameraInfo(l_info, r_info);
-    const Mat P(3,4, CV_64FC1, const_cast<double*>(l_info->P.data()));
+    const Mat P(3,4, CV_64FC1, const_cast<double*>(img_info->P.data()));
     camera_matrix_ = P.colRange(Range(0,3)).clone();
-
-    // Set the stereo model for the stereo properties
-    stereo_prop_.setCameraModel(stereo_camera_model);
 
     // Do not repeat
     first_iter_ = false;
   }
 
   // Set the images
-  stereo_prop_.setLeftImg(l_ptr->image);
-  stereo_prop_.setRightImg(r_ptr->image);
+  img_prop_.setImg(img_ptr->image);
 
   // Estimate the transform
   tf::Transform est_tf;
@@ -84,8 +79,8 @@ void template_pose::TemplatePoseBase::msgsCallback(
 
   // Publish the transform
   tf_broadcaster_.sendTransform(
-    tf::StampedTransform(camera_to_template_, l_img->header.stamp,
-    params_.stereo_frame_id, params_.template_frame_id));
+    tf::StampedTransform(camera_to_template_, img->header.stamp,
+    params_.frame_id, params_.template_frame_id));
 
   // Log
   double x, y, z, roll, pitch, yaw;
@@ -103,31 +98,33 @@ void template_pose::TemplatePoseBase::msgsCallback(
   */
 void template_pose::TemplatePoseBase::readParameters()
 {
-  Params template_pose_params;
+  Params params;
 
   // General parameters
-  nh_private_.getParam("queue_size", template_pose_params.queue_size);
-  nh_private_.getParam("desc_threshold", template_pose_params.desc_threshold);
-  nh_private_.getParam("min_matches", template_pose_params.min_matches);
-  nh_private_.getParam("min_inliers", template_pose_params.min_inliers);
-  nh_private_.param("template_image_name", template_pose_params.template_image_name, std::string("template.png"));
-  nh_private_.param("stereo_frame_id", template_pose_params.stereo_frame_id, std::string("/stereo_down"));
-  nh_private_.param("template_frame_id", template_pose_params.template_frame_id, std::string("/template_image"));
+  nhp_.param("queue_size", params.queue_size, 2);
+  nhp_.param("desc_threshold", params.desc_threshold, 0.8);
+  nhp_.param("min_matches", params.min_matches, 40);
+  nhp_.param("min_inliers", params.min_inliers, 20);
+  nhp_.param("template_image_name", params.template_image_name, std::string("template.png"));
+  nhp_.param("frame_id", params.frame_id, std::string("/camera"));
+  nhp_.param("template_frame_id", params.template_frame_id, std::string("/template_image"));
+  nhp_.param("desc_type", params.desc_type, std::string("SIFT"));
+  nhp_.param("bucket_width", params.bucket_width, 30);
+  nhp_.param("bucket_height", params.bucket_height, 30);
+  nhp_.param("max_bucket_features", params.max_bucket_features, 10);
+  nhp_.param("template_width", params.template_width, 0.5);
+  nhp_.param("template_height", params.template_height, 0.16);
 
   // Set stereo slam parameters
-  setParams(template_pose_params);
+  setParams(params);
 
   // Topics subscriptions
-  string left_topic, right_topic, left_info_topic, right_info_topic;
-  nh_private_.param("left_topic", left_topic, string("/left/image_rect_color"));
-  nh_private_.param("right_topic", right_topic, string("/right/image_rect_color"));
-  nh_private_.param("left_info_topic", left_info_topic, string("/left/camera_info"));
-  nh_private_.param("right_info_topic", right_info_topic, string("/right/camera_info"));
+  string image_topic, image_info_topic;
+  nhp_.param("image_topic", image_topic, string("/left/image_rect_color"));
+  nhp_.param("image_info_topic", image_info_topic, string("/left/camera_info"));
   image_transport::ImageTransport it(nh_);
-  left_sub_ .subscribe(it, left_topic, 1);
-  right_sub_.subscribe(it, right_topic, 1);
-  left_info_sub_.subscribe(nh_, left_info_topic, 1);
-  right_info_sub_.subscribe(nh_, right_info_topic, 1);
+  image_sub_.subscribe(it, image_topic, 1);
+  info_sub_.subscribe(nh_, image_info_topic, 1);
 }
 
 /** \brief Initializes the template pose class
@@ -139,14 +136,6 @@ bool template_pose::TemplatePoseBase::initialize()
   first_iter_ = true;
   camera_to_template_.setIdentity();
 
-  // Initialize image properties objects
-  template_pose::ImageProperties::Params image_params;
-  image_params.desc_type = "SIFT";
-  image_params.desc_threshold = params_.desc_threshold;
-  image_params.epipolar_threshold = 3.5;
-  template_prop_.setParams(image_params);
-  stereo_prop_.setParams(image_params);
-
   // Check if template image exists
   string template_file =  ros::package::getPath(ROS_PACKAGE_NAME) + 
                           string("/etc/") + params_.template_image_name;
@@ -157,34 +146,46 @@ bool template_pose::TemplatePoseBase::initialize()
     return false;
   }
 
-  // Read the template image and extract kp and descriptors
+  // Read the template image
   Mat img_temp = imread(template_file, CV_LOAD_IMAGE_COLOR);
-  template_prop_.setLeftImg(img_temp);
+
+  // Initialize image properties objects
+  template_pose::ImageProperties::Params image_params;
+  image_params.desc_type = params_.desc_type;
+  image_params.bucket_width = params_.bucket_width;
+  image_params.bucket_height = params_.bucket_height;
+  image_params.max_bucket_features = params_.max_bucket_features;
+  image_params.px_meter_x = (float)img_temp.cols / (float)params_.template_width;
+  image_params.px_meter_y = (float)img_temp.rows / (float)params_.template_height;
+
+  // Set the image properties
+  template_prop_.setParams(image_params);
+  img_prop_.setParams(image_params);
+
+  // Compute template properties
+  template_prop_.setImg(img_temp);
+  template_prop_.compute3D();
 
   // Callback synchronization
   bool approx;
-  nh_private_.param("approximate_sync", approx, false);
+  nhp_.param("approximate_sync", approx, false);
   if (approx)
   {
     approximate_sync_.reset(new ApproximateSync(ApproximatePolicy(params_.queue_size),
-                                    left_sub_, 
-                                    right_sub_, 
-                                    left_info_sub_, 
-                                    right_info_sub_) );
+                                    image_sub_,
+                                    info_sub_) );
     approximate_sync_->registerCallback(boost::bind(
         &template_pose::TemplatePoseBase::msgsCallback,
-        this, _1, _2, _3, _4));
+        this, _1, _2));
   }
   else
   {
     exact_sync_.reset(new ExactSync(ExactPolicy(params_.queue_size),
-                                    left_sub_, 
-                                    right_sub_, 
-                                    left_info_sub_, 
-                                    right_info_sub_) );
+                                    image_sub_,
+                                    info_sub_) );
     exact_sync_->registerCallback(boost::bind(
         &template_pose::TemplatePoseBase::msgsCallback, 
-        this, _1, _2, _3, _4));
+        this, _1, _2));
   }
 
   return true;
@@ -200,11 +201,11 @@ bool template_pose::TemplatePoseBase::estimateTransform(tf::Transform& output)
   output.setIdentity();
 
   // Crosscheck descriptors matching
-  Mat desc_stereo = stereo_prop_.getLeftDesc3D();
-  Mat desc_template = template_prop_.getLeftDesc();
+  Mat desc_image = img_prop_.getDesc();
+  Mat desc_template = template_prop_.getDesc();
   vector<DMatch> matches;
   Mat match_mask;
-  template_pose::OpencvUtils::crossCheckThresholdMatching(desc_stereo,
+  template_pose::OpencvUtils::crossCheckThresholdMatching(desc_image,
   desc_template, params_.desc_threshold, match_mask, matches);
 
   ROS_INFO_STREAM("[TemplatePose:] Found " << matches.size() <<
@@ -215,17 +216,17 @@ bool template_pose::TemplatePoseBase::estimateTransform(tf::Transform& output)
     return false;
 
   // Extract keypoints and 3d points of vertex i and j
-  vector<KeyPoint> kp_template = template_prop_.getLeftKp();
-  vector<Point3f> points3d_stereo = stereo_prop_.get3Dpoints();
+  vector<KeyPoint> kp_image = img_prop_.getKp();
+  vector<Point3f> points3d_template = template_prop_.get3Dpoints();
 
   vector<Point2f> matched_keypoints;
   vector<Point3f> matched_3d_points;
   for (size_t i = 0; i < matches.size(); ++i)
   {
-    int idx_stereo = matches[i].queryIdx;
+    int idx_image = matches[i].queryIdx;
     int idx_template = matches[i].trainIdx;
-    matched_3d_points.push_back(points3d_stereo[idx_stereo]);
-    matched_keypoints.push_back(kp_template[idx_template].pt);
+    matched_3d_points.push_back(points3d_template[idx_template]);
+    matched_keypoints.push_back(kp_image[idx_image].pt);
   }
   
   // Compute the transformation between the vertices
@@ -233,7 +234,7 @@ bool template_pose::TemplatePoseBase::estimateTransform(tf::Transform& output)
   vector<int> inliers;
   solvePnPRansac(matched_3d_points, matched_keypoints, camera_matrix_, 
                  Mat(), rvec, tvec, false /* no extrinsic guess */,
-                 50 /* iterations */, 8.0 /* reproj. error */,
+                 100 /* iterations */, 4.0 /* reproj. error */,
                  params_.min_inliers /* min inliers */, inliers);
 
   ROS_INFO_STREAM("[TemplatePose:] Found " << inliers.size() <<
@@ -253,6 +254,6 @@ bool template_pose::TemplatePoseBase::estimateTransform(tf::Transform& output)
   tf::Vector3 translation(tvec.at<double>(0, 0), tvec.at<double>(1, 0), 
       tvec.at<double>(2, 0));
 
-  output = (tf::Transform(quaternion, translation)).inverse();
+  output = tf::Transform(quaternion, translation);
   return true;
 }
